a28aab2caff1b307059b817b91c0ad0c20982d1a
Louise Iyengar <louise.iyengar@flatironschool.com>
2022-02-03 16:20:33 -0500

    commit tree 22da7efe1e424abf9c6d54acaeef1c430138893f

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+
+  const [inCart, setInCart] = useState(false)
+
+
+  function handleCartAdd() {
+    setInCart((inCart)=> !inCart)
+  } 
+
+  const cartClass = inCart ? "in-cart" : ""
+  const cartText = inCart ?  "Remove from Cart" : "Add to Cart"
+
   return (
-    <li className="">
+    <li className={cartClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={inCart ? 'remove' : 'add'} onClick={handleCartAdd}>{cartText}</button>
     </li>
   );
 }
